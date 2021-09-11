378159645ede78b22584c3dfbdeec99f5eca4301
gurjit singh <gurjitmehta@gmail.com>
2021-09-12 01:12:03 +0530

    feat: adds ProjectCard tests

--- src/components/ProjectCard/ProjectCard.test.tsx
+++ src/components/ProjectCard/ProjectCard.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import projectMock from '../../mock/project.mock';
 import ProjectCard from './ProjectCard'
 
-test('renders project card properly', () => {
-    render(<ProjectCard />)
-    const linkElement = screen.getByText(/learn react/i)
-    expect(linkElement).toBeInTheDocument()
+describe('<ProjectCard />', () => {
+    it('should render properly', () => {
+        const { asFragment } = render(<ProjectCard {...projectMock} onActionClick={() => { }} />)
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should call the onActionClick properly', () => {
+        const onActionClick = jest.fn()
+        render(<ProjectCard {...projectMock} onActionClick={onActionClick} />)
+        const viewButton = screen.getByText('view');
+        fireEvent.click(viewButton)
+        expect(onActionClick).toBeCalledTimes(1);
+    })
 })
+
